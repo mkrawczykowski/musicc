f4dde84029e5ae11a0d371ce7803d16721d9cbaf
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-04-05 15:31:40 +0200

    added ignoring to pugecss

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,11 +44,14 @@ module.exports = {
         printRejected: true, // Print removed selectors and processed file names
         // develop: true, // Enable while using `gatsby develop`
         // tailwind: true, // Enable tailwindcss support
-        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        ignore: ['/node_modules/vanilla-cookieconsent/dist/cookieconsent.css'], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
         purgeCSSOptions: {
           // https://purgecss.com/configuration.html#options
-          // safelist: ['safelist'], // Don't remove this selector
+          safelist: {
+
+          }
+          ['safelist'], // Don't remove this selector
         },
         // More options defined here https://purgecss.com/configuration.html#options
       },
